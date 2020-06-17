eaa40a71c4888301d5f01765436dbe7e383cfb0c
Sathish Vadakattu <sathish.vadakattu@solugenix.com>
2020-06-17 21:14:25 +0530

    Initial Commit

---
+++ SampleSwiftProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleSwiftProject
+//
+//  Created by Solugenix India on 17/06/20.
+//  Copyright © 2020 Solugenix India. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
