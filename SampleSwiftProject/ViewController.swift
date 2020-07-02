5e7ee0bb8b5661caff0849119e4d0f67e3bf652d
Sathish Vadakattu <sathish.vadakattu@solugenix.com>
2020-07-02 15:04:58 +0530

    second commit

--- SampleSwiftProject/ViewController.swift
+++ SampleSwiftProject/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("afshhasd")
+        print("asdfsdaf")
         // Do any additional setup after loading the view.
     }
 
