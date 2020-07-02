6a2309363e7aad6164487c18b51b9088b03c86b9
Sathish Vadakattu <sathish.vadakattu@solugenix.com>
2020-07-02 17:54:48 +0530

    new change again

--- SampleSwiftProject/ViewController.swift
+++ SampleSwiftProject/ViewController.swift
@@ -16,6 +16,9 @@ class ViewController: UIViewController {
         print("asdfsdaf")
         print("asa")
         
+        
+        print("master code")
+        
         // Do any additional setup after loading the view.
     }
 
