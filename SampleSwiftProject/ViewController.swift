fc1851de08df0ebceebae709829b01a89656e9ff
Sathish Vadakattu <sathish.vadakattu@solugenix.com>
2020-07-02 16:50:12 +0530

    added changes in branch 1

--- SampleSwiftProject/ViewController.swift
+++ SampleSwiftProject/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("branch1")
         // Do any additional setup after loading the view.
     }
 
