fa9301542e1afb302d238d410a41d893a988fd52
Sathish Vadakattu <sathish.vadakattu@solugenix.com>
2020-07-06 16:26:28 +0530

    aa

--- SampleSwiftProject/ViewController.swift
+++ SampleSwiftProject/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("a")
                        
         // Do any additional setup after loading the view.
     }
