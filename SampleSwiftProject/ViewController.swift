3b608615a92338717b744edfd1726c1b725d8d8b
Sathish Vadakattu <sathish.vadakattu@solugenix.com>
2020-07-06 19:24:38 +0530

    dds

--- SampleSwiftProject/ViewController.swift
+++ SampleSwiftProject/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("s")
         // Do any additional setup after loading the view.
     }
 
