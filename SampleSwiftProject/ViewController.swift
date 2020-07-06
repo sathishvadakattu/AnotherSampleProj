17f5e40d7319378ab6490adc23149f0e929ec6c1
Sathish Vadakattu <sathish.vadakattu@solugenix.com>
2020-07-06 15:41:31 +0530

    first Commit

--- SampleSwiftProject/ViewController.swift
+++ SampleSwiftProject/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        print("master code")
+        print("master1")
                 
         // Do any additional setup after loading the view.
     }
